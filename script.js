a97033fd6b7a7fc49dddbc6efe8566b7115b26fe
Giovanni Amighetti <amighetti214@hotmail.com>
2020-03-11 17:49:34 -0600

    add script file
    add script file

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// Have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
+
+var str = 'fun times!';
+
+function LetterChanges(str) {
+  
+    str = str.trim().toLowerCase();
+    var len = str.length;
+    var newStr = '';
+    
+    for (var i = 0; i < len; i++) {
+      if (/[a-ce-gi-mo-su-y]/.test(str[i])) {
+        newStr += String.fromCharCode(((str[i].charCodeAt(0) - 18) % 26) + 97)    
+      }
+      else if (/[zdhnt]/.test(str[i])) {
+          newStr += String.fromCharCode(((str[i].charCodeAt(0) - 18) % 26) + 65);
+      }
+      else {
+       newStr += str[i]; 
+      }
+    }
+    return newStr; 
+           
+}
+LetterChanges(str);
